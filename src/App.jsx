29b669712365df22bd8b3fd9a04068add9fbca1e
melabreu <melissa.abreu84@gmail.com>
2025-09-18 13:33:27 -0400

    added button 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Button1 from './components/example-1-normal-css/Button.jsx'
 import Button2 from './components/example-2-inline-css/Button.jsx'
 import Button3 from './components/example-3-styled-components/Button.jsx'
+import Button4 from './components/example-4-css-modules/Button.jsx'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Button1 buttonText="Example 1" />
       <Button2 buttonText="Example 2" />
       <Button3 buttonText="Example 3" />
+      <Button4 buttonText="Example 4" />
     </div>
   );
 };
