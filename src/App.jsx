4f402cc2afbd72494d93a5e26ada2fe88e7304c6
melabreu <melissa.abreu84@gmail.com>
2025-09-18 13:23:54 -0400

    added button 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Button1 from './components/example-1-normal-css/Button.jsx'
 import Button2 from './components/example-2-inline-css/Button.jsx'
+import Button3 from './components/example-3-styled-components/Button.jsx'
 
 const App = () => {
   return (
     <div className="App">
       <Button1 buttonText="Example 1" />
       <Button2 buttonText="Example 2" />
+      <Button3 buttonText="Example 3" />
     </div>
   );
 };
